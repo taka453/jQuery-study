e4c1b7c3f3d221e6c5969b41aabb309351d87cbe
sugu3 <teddy.picker.505@gmail.com>
2020-06-16 17:23:47 +0900

    first commit

---
+++ js/scroll_top.js
@@ -0,0 +1,27 @@
+// DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
+$(function () {
+  // トップへ戻るボタンのエレメントを取得
+  const scrollTop = $('.scroll-top');
+  // ボタンをクリックしたらページトップまで戻るを実装
+  $('span').on('click', function () {
+    // スクロールの速度(ミリ秒)
+    let scrollSpeed = 300;
+    // スムーススクロールを実行
+    $("html,body").animate({scrollTop : 0}, scrollSpeed);
+  });
+
+  // windowのスクロール位置でボタンの表示・非表示
+  $(window).on('scroll', function () {
+    // スクロールの速度(ミリ秒)
+    let scrollSpeed = 300;
+    // 現在のスクロール位置を取得(windowのscrollTopを取得)
+    const windowScrolltop = $(window).scrollTop();
+    // もし現在のスクロール位置が30を超えていたらトップへ戻るボタンのエレメントをfadeInで表示
+    // 30を超えていない場合はfadeOutで非表示
+    if(windowScrolltop > 30) {
+      scrollTop.fadeIn();
+    } else {
+      scrollTop.fadeOut();
+    }
+  });
+});
---
+++ js/select.js
@@ -0,0 +1,44 @@
+// DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
+$(function () {
+  // 未選択のセレクトボックス
+  const $noneSelected = $('#none-selected-items');
+
+  // 選択済みのセレクトボックス
+  const $selected = $('#selected-items');
+
+  // 右に移動
+  $('.js-item-to-right').on('click', function () {
+    // 選択されているoption(option:selected)を取得
+    let option = $('#none-selected-items option:selected');
+    // 選択を解除
+    $(option).prop('selected', false);
+    // 選択済みのセレクトボックスに移動
+    $($selected).append(option);
+  });
+
+  // 右に全て移動
+  $('.js-item-to-right-all').on('click', function () {
+    // 全てのoptionを取得
+    let option = $('#none-selected-items');
+    // 選択済みのセレクトボックスに移動
+    $($selected).after(option);
+  });
+
+  // 左に移動
+  $('.js-item-to-left').on('click', function () {
+    // 選択されているoption(option:selected)を取得
+    let option = $('#selected-items option:selected');
+    // 選択を解除
+    $(option).prop('selected', false);
+    // 未選択のセレクトボックスに移動
+    $($noneSelected).append(option);
+  });
+
+  // 左に全て移動
+  $('.js-item-to-left-all').on('click', function () {
+    // 全てのoptionを取得
+    let option = $('#selected-items');
+    // 未選択のセレクトボックスに移動
+    $($noneSelected).after(option);
+  });
+});
