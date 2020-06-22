67ce9bdd5297a1ba502c9e7073c0d58fc62820c2
taka453 <paisley.park.39@gmail.com>
2020-06-22 11:05:27 +0900

    復習のため再度制作

--- jQuery-introduction/tab/main.js
+++ jQuery-introduction/tab/main.js
@@ -1,22 +1,41 @@
-$(function() {
+$(function(){
 
     $('ul').on('click', 'li', function(){
-        //要素を設定する
-        $('li.selsected').removeClass('selected');
-        //クリックされた要素をON状態にする
-        $(this).addClass('selsected');
-        //クリックされた要素のid属性の値を取得
+        $('li.selected').removeClass('selected');
+        $(this).addClass('selected');
+
         myID = this.id.split('_')[1];
-        //先に指定した親要素にある子要素
         $('#textArea > div').hide();
         $('#text_' + myID).show();
 
-        //タブの色を取得
         const myColor = $(this).css('background-color');
-        $('textArea').css('background-color', myColor);
+        $('#textArea').css('background-color', myColor);
     });
 
-    //イベントが設定した後に設定する。
     $('#tab_1').trigger('click');
+
+
 });
+
+// $(function() {
+
+//     $('ul').on('click', 'li', function(){
+//         //要素を設定する
+//         $('li.selsected').removeClass('selected');
+//         //クリックされた要素をON状態にする
+//         $(this).addClass('selsected');
+//         //クリックされた要素のid属性の値を取得
+//         myID = this.id.split('_')[1];
+//         //先に指定した親要素にある子要素
+//         $('#textArea > div').hide();
+//         $('#text_' + myID).show();
+
+//         //タブの色を取得
+//         const myColor = $(this).css('background-color');
+//         $('textArea').css('background-color', myColor);
+//     });
+
+//     //イベントが設定した後に設定する。
+//     $('#tab_1').trigger('click');
+// });
 
